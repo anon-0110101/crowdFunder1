a3a8726ef2ed4516c154725328b34a3a98614a96
anon-0110101 <131133702+anon-0110101@users.noreply.github.com>
2023-08-29 20:59:56 -0600

    Add files via upload

---
+++ dist/assets/hasERC20Allowance-cfc83325.browser.esm.57b23f68.js
@@ -0,0 +1,1 @@
+import{p as c,az as d}from"./index.4868755f.js";async function w(a,e,n){const r=a.getProvider(),s=new c(r,e,d,{},a.storage),t=await a.getSignerAddress(),o=a.readContract.address;return(await s.readContract.allowance(t,o)).gte(n)}export{w as h};
